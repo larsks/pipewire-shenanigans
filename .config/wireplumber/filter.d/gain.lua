dc7039ca87c09222b40f1abd16eae42fa29c56ab
Lars Kellogg-Stedman <lars@oddbit.com>
2026-03-09 21:07:40 -0400

    Add gain plugin (currently unused)

---
+++ .config/wireplumber/filter.d/gain.lua
@@ -0,0 +1,33 @@
+local args_string = [[
+{
+  "media.name": "Gain (Mono)",
+  "filter.graph": {
+    "nodes": [
+      {
+        "type": "builtin",
+        "name": "gain",
+        "label": "mixer",
+        "control": {
+          "Gain 1": 8
+        }
+      }
+    ],
+    "inputs": [ "gain:In 1" ],
+    "outputs": [ "gain:Out" ]
+  },
+  "capture.props": {
+    "node.name": "gain.input",
+    "media.class": "Audio/Sink"
+  },
+  "playback.props": {
+    "node.name": "gain.output",
+    "media.class": "Audio/Source"
+  },
+  "node.passive": true,
+  "audio.position": [ "MONO" ],
+  "node.description": "Gain (Mono)",
+  "audio.channels": 1
+}
+]]
+local properties = {}
+filter_chain = LocalModule("libpipewire-module-filter-chain", args_string, properties)
